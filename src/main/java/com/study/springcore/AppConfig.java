5615869c0a10c39178b1c3d10fd079639399c83a
it1457 <cgw981@naver.com>
2021-07-23 18:34:20 +0900

    Refactoring AppConfig

--- src/main/java/com/study/springcore/AppConfig.java
+++ src/main/java/com/study/springcore/AppConfig.java
@@ -1,6 +1,8 @@
 package com.study.springcore;
 
+import com.study.springcore.discount.DiscountPolicy;
 import com.study.springcore.discount.FixDiscountPolicy;
+import com.study.springcore.discount.RateDiscountPolicy;
 import com.study.springcore.member.MemberService;
 import com.study.springcore.member.MemberServiceImpl;
 import com.study.springcore.member.MemoryMemberRepository;
@@ -10,10 +12,19 @@ import com.study.springcore.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    private MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
     }
+
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+    }
+
 }
