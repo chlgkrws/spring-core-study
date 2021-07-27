b63ad4a9d24ce0634e9d91579745a796379b8108
it1457 <cgw981@naver.com>
2021-07-28 01:56:09 +0900

    Test ComponentScan Filters

---
+++ src/test/java/com/study/springcore/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package com.study.springcore.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
